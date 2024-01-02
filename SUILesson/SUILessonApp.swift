761b4dd9c7bf5fe76fd0b2f365216c55342280b2
AsetB <asetbakirov@gmail.com>
2024-01-02 11:57:16 +0600

    Initial Commit

---
+++ SUILesson/SUILessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUILessonApp.swift
+//  SUILesson
+//
+//  Created by Aset Bakirov on 02.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SUILessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
